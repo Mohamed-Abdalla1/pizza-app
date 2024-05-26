326216ffadf51602287bbbdf3f2dc0c825c6b330
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-05-26 09:10:30 +0300

    create firebase pizza repo

---
+++ packages/pizza_repository/lib/src/firebase_pizza_repo.dart
@@ -0,0 +1,25 @@
+
+
+import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pizza_repository/src/entities/entity.dart';
+import 'package:pizza_repository/src/models/models.dart';
+
+import 'pizza_repo.dart';
+
+class FirebasePizzaRepo implements PizzaRepo {
+  final pizzaCollection = FirebaseFirestore.instance.collection('pizzas');
+
+  Future<List<PizzaModel>> getPizzas() async {
+    try {
+      return await pizzaCollection.get().then((value) => value.docs
+          .map((value) =>
+              PizzaModel.fromEntity(PizzaEntity.fromDocument(value.data())))
+          .toList());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
